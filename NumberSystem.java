6a9d64ab8383db795220f8ffee00a77e7664ced3
gukiruthika <112635198+gukiruthika@users.noreply.github.com>
2022-10-18 21:55:03 +0530

    Add files via upload

--- NumberSystem.java
+++ NumberSystem.java
@@ -19,14 +19,14 @@ public class NumberSystem {
 		List<String> numberList = new ArrayList<String>();
 		numberList.add("3");
 		numberList.add("4");
-		for (int j = 0; j < numberList.size(); j++) {
-			numberList.add(numberList.get(j) + "3");
-			numberList.add(numberList.get(j) + "4");
+		for (int i = 0; i < numberList.size(); i++) {
 			if (numberList.size() >= n)
 				break;
+			numberList.add(numberList.get(i) + "3");
+			numberList.add(numberList.get(i) + "4");
 		}
 
-		return n+"th term -> "+numberList.get(n - 1);
+		return n + "th term -> " + numberList.get(n - 1);
 
 	}
 
