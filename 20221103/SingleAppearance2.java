37185563783f8501e8705646b4970f31df965af6
gukiruthika <112635198+gukiruthika@users.noreply.github.com>
2022-11-04 12:35:14 +0530

    Add files via upload

--- 20221103/SingleAppearance2.java
+++ 20221103/SingleAppearance2.java
@@ -7,7 +7,8 @@ public class SingleAppearance2 {
 	public static void main(String[] args) {
 		new SingleAppearance2().process();
 	}
-
+	
+	static int timeComplexity=0;
 	private void process() {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter number of elements");
@@ -17,24 +18,21 @@ public class SingleAppearance2 {
 			arr[i] = scan.nextInt();
 		}
 		scan.close();
-		System.out.println(getSingleAppearance(arr, 0, arr.length));
+		System.out.println(getSingleAppearance(arr, 0, arr.length)+" - count -> "+count);
 
 	}
 
 	int getSingleAppearance(int[] arr, int start, int end) {
-
-		if (end - start == 1) {
-			return arr[start];
+		timeComplexity++;
+		if (end - start < 3) {
+			return arr[end-1];
 		}
 		int mid = start + (end - start) / 2;
-
-		if (arr[mid] != arr[mid - 1] && arr[mid] != arr[mid + 1]) {
+		if (arr[mid] != arr[mid - 1] && arr[mid] != arr[mid + 1])
 			return arr[mid];
-		} else if ((arr[mid] == arr[mid - 1] && mid % 2 == 0) || (arr[mid] == arr[mid + 1] && mid % 2 != 0)) {
+		else if ((arr[mid] == arr[mid - 1] && mid % 2 == 0) || (arr[mid] == arr[mid + 1] && mid % 2 != 0))
 			return getSingleAppearance(arr, start, mid);
-		} else {
-			return getSingleAppearance(arr, mid + 1, end);
-		}
-
+		else
+			return getSingleAppearance(arr, mid, end);
 	}
 }
