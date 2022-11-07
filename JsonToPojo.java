823f3f6066755fc7ddba4c8d940ab09d4482610f
gukiruthika <112635198+gukiruthika@users.noreply.github.com>
2022-11-07 14:33:18 +0530

    Add files via upload

---
+++ JsonToPojo.java
@@ -0,0 +1,18 @@
+package assignment;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonToPojo {
+
+	public static void main(String[] args) {
+		new JsonToPojo().process();
+	}
+
+	private void process() {
+		ObjectMapper obj = new ObjectMapper();
+		
+		
+	}
+
+	
+}
